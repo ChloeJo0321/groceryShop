577fcc55ffca1d08d7893b7533a642fd766327b0
Chloe Jo <hyeyoonjo321@gmail.com>
2025-11-13 13:37:13 -0500

    Modify the variable for userRoute file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express"); //require("express") = Node.js module
 const app = express();
 const port = 3000;
 const productRouter = require("./routes/productRoute");
-const userRouter = require("./routes/userRoutes");
+const { router: userRouter } = require("./routes/userRoute");
 const cartRouter = require("./routes/cartRoute");
 const cookieParser = require("cookie-parser");
 const { authenticateUser } = require("./middleware/authMiddleware");
@@ -12,14 +12,13 @@ app.use(express.urlencoded({ extended: true })); //Middleware call for form
 app.use(express.json()); //Middleware call for form
 app.use(cookieParser());
 
-// Check the login status first
-app.get("/", authenticateUser, (req, res) => {
-  // Already login
+// Check the sign in status first
+app.get(["/", "/index.html"], authenticateUser, (req, res) => {
+  // Already sign in
   if (req.isAuthenticated) {
     return res.redirect("/welcome.html");
   }
-
-  // Not login yet
+  // Not sign in yet
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
